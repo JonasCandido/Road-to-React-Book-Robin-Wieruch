8b705035df6c9e1864f6d68c0c6913d5427bbb4a
JonasCandido <jonasdcandido210@gmail.com>
2023-10-30 21:59:37 -0300

    eslint updated

--- hacker-stories/src/App.jsx
+++ hacker-stories/src/App.jsx
@@ -51,16 +51,19 @@ const App = () => {
 
   const handleFetchStories = React.useCallback(async() => { 
     dispatchStories({type:storiesFecthInit});
-    const result = await axios.get(url);
-    dispatchStories({
-      type:storiesFetchSuccess,
-      payload: result.data.hits,
-    });
+    try{
+      const result = await axios.get(url);
+      dispatchStories({
+        type:storiesFetchSuccess,
+        payload: result.data.hits,
+      });}catch{
+        dispatchStories({type:storiesFetchFailure});
+      }
     },[url]);
 
   React.useEffect(()=>{
-    handleFetchStories();//C
-  },[handleFetchStories]);//D 
+    handleFetchStories();
+  },[handleFetchStories]);
 
   const handleRemoveStory = (item) => {
     dispatchStories({
@@ -73,15 +76,15 @@ const App = () => {
     setSearchTerm(event.target.value);
   };
 
-  const handleSearchSubmit = () => {
+  const handleSearchSubmit = (event) => {
     setUrl(`${API_ENDPOINT}${searchTerm}`);
+    event.preventDefault();
   };
 
   return(
     <div>
       <h1>My Hacker Stories</h1>
-      <InputWithLabel id="search" value={searchTerm} isFocused onInputChange={handleSearchInput}><strong>Search:</strong></InputWithLabel>
-      <button type="button" disabled={!searchTerm} onClick={handleSearchSubmit}>Submit</button>
+      <SearchForm searchTerm={searchTerm} onSearchInput={handleSearchInput} onSearchSubmit={handleSearchSubmit}/>
       <hr />
       {stories.isError && <p>Something went wrong...</p>}
       {stories.isLoading?(
@@ -90,6 +93,19 @@ const App = () => {
     </div>
   ); 
 }; 
+
+const SearchForm = ({searchTerm,onSearchInput,onSearchSubmit,}) => {
+  return(
+    <form onSubmit={onSearchSubmit}>
+      <InputWithLabel id="search" value={searchTerm} isFocused onInputChange={onSearchInput}>
+        <strong>Search:</strong>
+      </InputWithLabel>
+      <button type="submit" disabled={!searchTerm}>
+        Submit
+      </button>
+    </form>
+  );
+};
 
 const InputWithLabel = ({id,value, type='text',onInputChange,isFocused,children,}) => {
   const handleBlur = (event) => {
@@ -135,6 +151,12 @@ const Item = ({item, onRemoveItem}) => {
         <span><button type="button" onClick={() => onRemoveItem(item)}>Dismiss</button></span>
     </li>
   );
+};
+
+SearchForm.propTypes = {
+  searchTerm: PropTypes.string.isRequired,
+  onSearchInput: PropTypes.func.isRequired,
+  onSearchSubmit: PropTypes.func.isRequired,
 };
 
 InputWithLabel.propTypes = {
